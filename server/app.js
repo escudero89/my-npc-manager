101903b9255942d02fa977bae51507c3b15943d1
Cristian Escudero <cristian.escudero@mercadolibre.com>
2017-04-27 19:11:37 -0300

    Change hosted path in the package.json

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(path.join(__dirname, '../public/index.html')));
+  res.sendFile(path.resolve(path.join(__dirname, '../build/index.html')));
 });
 
 module.exports = app;
