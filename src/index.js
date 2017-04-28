52b5f2de46ff1d44f60927eecda0535fe6e1b6e0
Cristian Escudero <escudero89@gmail.com>
2017-04-27 22:42:27 -0300

    Add material-ui to the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Routes from './routes';
 
 import './index.css';
 
+// Needed for onTouchTap (material-ui)
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// Renders the DOM
 ReactDOM.render(
   <Routes />,
   document.getElementById('root')
--- src/routes.js
+++ src/routes.js
@@ -5,28 +5,32 @@ import {
   Link,
   Switch
 } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Home from './components/Home';
 import About from './components/About';
 import NotFound from './components/NotFound';
 
+import MainAppBar from './mui/MainAppBar';
+
 const Routes = (props) => (
-  <Router {...props}>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-
-      <hr/>
-
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </div>
-  </Router>
+  <MuiThemeProvider>
+    <Router {...props}>
+      <div>
+        <MainAppBar/>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        <hr/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route component={NotFound}/>
+        </Switch>
+      </div>
+    </Router>
+  </MuiThemeProvider>
 );
 
 export default Routes;
