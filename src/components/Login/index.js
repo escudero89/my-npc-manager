0f1d71a321982bc0bffc081d9266fc332966b8fa
Cristian Escudero <escudero89@gmail.com>
2017-04-30 18:25:35 -0300

    Start working with firebase. Need form to work

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 import Login from './Login';
 import './style.css';
 
 export default class LoginContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.auth = this.auth.bind(this);
+  }
+
+  auth(email, password) {
+    firebase.auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  }
   render() {
     const { className } = this.props;
-    return <Login className={className} />;
+    return <Login className={className} onSubmit={this.auth} />;
   }
 }
